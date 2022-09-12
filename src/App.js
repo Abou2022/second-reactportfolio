ad454fe82fab0c28d86d952f7845211e40f7b8c9
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-12 16:03:32 -0700

    added routes folder for navbar in the src folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is second react portfolio</h1>
+    </div>
+  );
 }
 
 export default App;
