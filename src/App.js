36bc6e2e55d0bd0571882a386ad75abff1f1dbaf
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-12 16:32:14 -0700

    import router inside the app and call each child route inside the app function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 //import React
+//This is a parent page we need to call all file inside here
 import React from "react";
 import "./index.css";
+//to call each child : import <child name> from "<location folder and file>"
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is second react portfolio</h1>
-    </div>
+    //Before we added Routes we need <></> then insite our Routes
+    <>
+      <Routes>
+        {/* for each route we need a path */}
+        {/* For the home page the path gonna be / it considere our first element */}
+        <Route path="/" element={<Home />} />
+        {/* For the Project page the path gonna be /Project it considere our first element */}
+        <Route path="/project" element={<Project />} />
+        {/* For the About page the path gonna be /About it considere our first element */}
+        <Route path="/about" element={<About />} />
+        {/* For the home page the path gonna be / it considere our first element */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+// short cut "rafce"
+
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
