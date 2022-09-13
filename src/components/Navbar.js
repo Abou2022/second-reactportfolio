54c011d1f7bda1ed9b0f04efb20627a07d01a7f8
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-12 18:14:08 -0700

    fix couple error to navabarstyles link and added some styles to navbarStyle and index.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-// import "./NavbarStyles.css";
+import "./NavbarStyles.css";
 
 import React from "react";
 import { Link } from "react-router-dom";
@@ -9,9 +9,19 @@ const Navbar = () => {
       <Link to="/">
         <h1>MyPortfolio</h1>
       </Link>
-      <ul>
+      {/* Create a class to style the navbar */}
+      <ul className="nav-menu">
         <li>
-          <Link></Link>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </div>
