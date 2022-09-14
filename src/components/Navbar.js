36bb35c051cb9a60ed83a82e17cbdcd35609f557
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-13 17:38:23 -0700

    update hambuger icons showing the menu when we click on change from navbar.js and navbarStyles.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
 import "./NavbarStyles.css";
 
-import React from "react";
+//import useState when used
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // to add icons I will import first
 // I have to precise what categorie of icons I added "fa"
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  // I will use useState to change icons hamberger state
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>MyPortfolio</h1>
       </Link>
       {/* Create a class to style the navbar */}
-      <ul className="nav-menu">
+      {/* add if condition to display menu when you click on burguer icons  */}
+      <ul className={click ? "nav-menu.active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -29,8 +35,14 @@ const Navbar = () => {
         </li>
       </ul>
       {/* Add icons bambuger bar */}
-      <div className="hamburger">
-        <FaBars size={20} style={{ color: "#fff" }} />
+      {/* hide the icons until we change the screen  */}
+      <div className="hamburger" onClick={handleClick}>
+        {/* added statement to avoid diplay both icons in the same time suit case */}
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
       </div>
     </div>
   );
