7b2d69ffd2581d2e96d9495beb1a7065485d48b0
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-13 18:45:17 -0700

    added couple img

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -3,7 +3,13 @@ import "./HeroImgStyles.css";
 import React from "react";
 
 const HeroImg = () => {
-  return <div>HeroImg</div>;
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" />
+      </div>
+    </div>
+  );
 };
 
 export default HeroImg;
