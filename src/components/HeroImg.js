e4ef6fb0eb024ba809e9497fe6afb64bc45ef57a
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-14 18:01:25 -0700

    added btn to the HeroImg.js and added img to the web site

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,12 +2,29 @@ import "./HeroImgStyles.css";
 
 import React from "react";
 
+//to add img I import image
+import MyImg from "../assets/HeroImg1.jpg";
+
+//import link when I used
+import { Link } from "react-router-dom";
+
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="into-img" />
+        <img className="into-img" src={MyImg} alt="MyImg" />
       </div>
+      <div className="content">
+        <p>Hello, I AM A FREELANCER</p>
+        <h1>FULL STACK DEVELOPPER</h1>
+      </div>
+      {/* I will use link because I want to make my btn clickable  */}
+      <Link to="/project" className="btn">
+        PROJECTS
+      </Link>
+      <Link to="/contact" className="btn btn-light">
+        CONTACT
+      </Link>
     </div>
   );
 };
