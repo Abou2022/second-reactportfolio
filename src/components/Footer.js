742d87c5f874490eb9ff165c95fd86c7c9b248c6
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-15 18:37:03 -0700

    added personal info address, phone number and email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,44 @@
 import "./FooterStyles.css";
 
 import React from "react";
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
 
-export const Footer = () => {
-  return <div>Footer</div>;
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>Kent, Washington</p>
+              <p>United States</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              +1 (253) 263 - 9001
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              syllabakary2002@gmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
+
+export default Footer;
