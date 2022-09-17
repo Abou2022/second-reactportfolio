b21d7f87dce70b1ed34079886a608d324d82e18f
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-16 17:44:25 -0700

    added HeroImgProjectsStyles

---
+++ src/components/projects/HeoImgProjects.js
@@ -0,0 +1,16 @@
+import "../projects/HeroImgProjectsStyles.css";
+
+import React from "react";
+
+const HeoImgProjects = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>This is a heading</h1>
+        <p>This is a text</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeoImgProjects;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import HeoImgProjects from "../components/projects/HeoImgProjects";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
+      <HeoImgProjects />
       <Footer />
     </div>
   );
