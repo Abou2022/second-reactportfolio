d8e79e4ed404cd12e588bf8b567fee9cc4013322
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-19 18:32:26 -0700

    all projects data added

--- src/components/myWorks/WorkCardData.js
+++ src/components/myWorks/WorkCardData.js
@@ -1,7 +1,43 @@
-import pro1 from "../assets/PetFinder.png"
+import pro1 from "../assets/PetFinder.png";
+import pro2 from "../assets/InMyFridge.png";
+import pro3 from "../assets/DayPlanner.png";
+import pro4 from "../assets/TechBlog.png";
+import pro5 from "../assets/CodeQuiz.png";
 
 const ProjectCardData = [
-                    {
-                                        imgsrc: pro1;
-                    }
-]
+  {
+    imgsrc: pro1,
+    title: "Pet Finder",
+    text: "Team project to help people to report or found their pet. Techniques used: HTML5, CSS3, JavaScript, Bootstrap, Express-handlebars, MySQL2, Sequelize, dotenv, Node.js, cloudinary, bcrypt, express-session, connect-session-sequelize",
+    view: "https://hidden-shore-04969.herokuapp.com/home",
+    source: "https://github.com/Abou2022/Lost-Pet-Project",
+  },
+  {
+    imgsrc: pro2,
+    title: "In my Fridje",
+    text: "Team project to build a webapp for users to have better use of the ingredients in their fridge, reduce waste, and to be able to try out new recipes while with the use of what they already have at home. Techniques used: HTML5, CSS3, JavaScript, jQuery, Materialize CSS, APIs",
+    view: "https://kimester.github.io/inMyFridge/",
+    source: "https://github.com/Abou2022/inMyFridge",
+  },
+  {
+    imgsrc: pro3,
+    title: "Day planner",
+    text: "Calendar application that allows a user to save events for each hour of the day. Help people with busy schedule to add important important events to a daily planner so that he/her can manage their time effectively. Techniques used: HTML5, CSS3, jQuery, Third-Party APIs",
+    view: "https://abou2022.github.io/WorkDaySchedule/",
+    source: "https://github.com/Abou2022/WorkDaySchedule",
+  },
+  {
+    imgsrc: pro4,
+    title: "Code Quiz",
+    text: "Develope to help Full Stack developer, this webapp is CodeQuiz challenge. It is a timed quiz JavaScript funtamental that stores high scores. You can gauge your progress compared to your peers. Techniques used: HTML5, CSS3, JavaScript, APIs",
+    view: "https://abou2022.github.io/codeQuiz/",
+    source: "https://github.com/Abou2022/codeQuiz",
+  },
+  {
+    imgsrc: pro5,
+    title: "Tech Blog",
+    text: "CMS-style blog site is a application where developers or anybody can publish their blog posts, edit and/or delete their posts as well. As a developer, you can publish articles, blogs posts and your thoughts and opinions. Techniques used: HTML5, CSS3, JavaScript, Bootstrap, Express-handlebars, MySQL2, Sequelize, dotenv, Node.js, express-session, connect-session-sequelize",
+    view: "https://afternoon-river-69859.herokuapp.com/",
+    source: "https://github.com/Abou2022/Tech-Blog",
+  },
+];
