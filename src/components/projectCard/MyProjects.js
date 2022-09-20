69f8b9d5fb182b86d257d44936f383de54fe1e90
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-19 17:33:08 -0700

    styles added to MyProjectStyles.css

---
+++ src/components/projectCard/MyProjects.js
@@ -0,0 +1,44 @@
+import "../projectCard/MyProjectStyles.css";
+
+import React from "react";
+
+import project1 from "../assets/Pet Finder.png";
+import { NavLink } from "react-router-dom";
+
+const MyProjects = () => {
+  return (
+    <div className="pro-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img src={project1} alt="lost pet" />
+          <h2 className="project-title">Pet Finder</h2>
+          <div className="pro-details">
+            <p>
+              Team project to help people to report or found their pet.
+              Techniques used: HTML5, CSS3, JavaScript, Bootstrap,
+              Express-handlebars, MySQL2, Sequelize, dotenv, Node.js,
+              cloudinary, bcrypt, express-session, connect-session-sequelize
+            </p>
+            <div className="pro-btns">
+              <NavLink
+                to="https://hidden-shore-04969.herokuapp.com/home"
+                className="btn"
+              >
+                Page
+              </NavLink>
+              <NavLink
+                to="https://github.com/Abou2022/Lost-Pet-Project"
+                className="btn"
+              >
+                Source
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyProjects;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,14 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import HeroImgProjects from "../components/heroProjects/HeroImgProjects";
+import MyProjects from "../components/projectCard/MyProjects";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImgProjects heading="PROJECTS." text="Some of my recent works" />
+      <MyProjects />
       <Footer />
     </div>
   );
