c343ab93bb5e50e865cb494889a3af7c000287bf
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-20 18:41:09 -0700

    contact component created and file added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,12 +3,14 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import HeroImgProjects from "../components/heroProjects/HeroImgProjects";
+import Form from "../components/contact/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImgProjects heading="CONTACT ME" text="Let have a chat" />
+      <Form />
       <Footer />
     </div>
   );
