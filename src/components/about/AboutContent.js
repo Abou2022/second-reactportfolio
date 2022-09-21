ff6c4e05845b87891001069e25bcd33fdd17db14
Bakary Sylla <syllabakary2002@gmail.com>
2022-09-20 17:53:15 -0700

    added more picture

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -1,8 +1,29 @@
 import "../about/AboutContentStyles.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AboutContent = () => {
-  return <div>AboutContent</div>;
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>Who I am?</h1>
+        <p>
+          Full-stack developer with a background in Computer Science,
+          specializing in the MERN stack, I create reponsive secure websites.
+        </p>
+        <Link to="/contact">
+          <button className="btn">CONTACT</button>
+        </Link>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutContent;
